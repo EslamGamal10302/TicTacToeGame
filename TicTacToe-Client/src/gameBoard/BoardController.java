3493e50d92c95d0dd2cd77a1224163d1f7472eba
mennaihab <48647501+mennaihab@users.noreply.github.com>
2022-12-30 23:59:32 +0200

    adding xo board screen

---
+++ TicTacToe-Client/src/gameBoard/BoardController.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameBoard;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author menna
+ */
+public class BoardController implements Initializable {
+
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private ImageView position_3;
+
+    @FXML
+    private ImageView position_6;
+
+    @FXML
+    private ImageView position_1;
+
+    @FXML
+    private ImageView position_5;
+
+    @FXML
+    private ImageView position_9;
+
+    @FXML
+    private ImageView position_8;
+
+    @FXML
+    private ImageView position_4;
+
+    @FXML
+    private ImageView position_2;
+
+    @FXML
+    private ImageView position_7;
+
+    @FXML
+    private ImageView player_1;
+
+    @FXML
+    private ImageView player_2;
+
+    @FXML
+    void backButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionEightClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionFiveClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionFourClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionNineClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionOneClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionSevenClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionSixClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionThreeClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void positionTwoClicked(MouseEvent event) {
+
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+      //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
+
+    
+  
